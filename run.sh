3c9ee8b19df36659075eb33ebaea2bc12f25aba7
Kien Truong <ktruong29@csu.fullerton.edu>
2021-05-16 18:56:08 -0700

    Modified run.sh proj2.out --> proj3.out

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Compiling all .cpp files"
-g++ -Wall *.cpp -o proj2.out
+g++ -Wall *.cpp -o proj3.out
 
-echo "Run the program: ./proj2.out <text file>"
+echo "Run the program: ./proj3.out <text file>"
